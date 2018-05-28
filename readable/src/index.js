e1a0bdd5e354bde63168936cd3f7fba62f7a2b20
ct <1768012206@qq.com>
2018-05-28 13:02:25 +0800

    first commit

--- readable/src/index.js
+++ readable/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux'
+import reducers from './reducers/reducers'
+import { Provider } from 'react-redux'
+import thunk from "redux-thunk"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk))
+
+ReactDOM.render((
+    <Provider store={store}>
+    <App/>
+    </Provider>
+), document.getElementById('root'))
 registerServiceWorker();
